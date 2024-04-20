b104abb85ed24550ce4d3506d27fec32e8d368cb
sopheavatey <2023363heang@aupp.edu.kh>
2024-04-20 13:13:39 +0700

    1

--- welcome.py
+++ welcome.py
@@ -3,7 +3,7 @@ import pygame
 import sys
 from pygame.locals import *
 from tkinter import *
-import easy, medium, hard
+import easy, medium, hard,experiment
 
 
 WINDOWWIDTH = 640 
@@ -37,21 +37,25 @@ def welcomeScreen():
         DISPLAYSURF.blit(titleText, titleRect)
 
         font = pygame.font.SysFont('arial', 28)
-        easyText = font.render("Easy 3x4 grid", True, (255, 255, 255))  # Use tuple for color
+        easyText = font.render("Easy", True, (255, 255, 255))  # Use tuple for color
         easyRect = easyText.get_rect()
         easyRect.center = (WINDOWWIDTH // 2, WINDOWHEIGHT // 2)
         DISPLAYSURF.blit(easyText, easyRect)
 
-        mediumText = font.render("Medium 4x4 grid", True, (255, 255, 255))  # Use tuple for color
+        mediumText = font.render("Medium", True, (255, 255, 255))  # Use tuple for color
         mediumRect = mediumText.get_rect()
         mediumRect.center = (WINDOWWIDTH // 2, WINDOWHEIGHT // 2 + 40)
         DISPLAYSURF.blit(mediumText, mediumRect)
 
-        hardText = font.render("Hard 6x6 grid", True, (255, 255, 255))  # Use tuple for color
+        hardText = font.render("Hard", True, (255, 255, 255))  # Use tuple for color
         hardRect = hardText.get_rect()
         hardRect.center = (WINDOWWIDTH // 2, WINDOWHEIGHT // 2 + 80)
         DISPLAYSURF.blit(hardText, hardRect)
 
+        exText = font.render("Experiment", True, (255, 255, 255))  # Use tuple for color
+        exRect = mediumText.get_rect()
+        exRect.center = (WINDOWWIDTH // 2, WINDOWHEIGHT // 2 + 120)
+        DISPLAYSURF.blit(exText, exRect)
         pygame.display.update()
 
         for event in pygame.event.get():
@@ -66,5 +70,7 @@ def welcomeScreen():
                     return medium.main()
                 elif hardRect.collidepoint(mousex, mousey):
                     return hard.main()
+                elif exRect.collidepoint(mousex, mousey):
+                    return experiment.main()
 
 welcomeScreen()
